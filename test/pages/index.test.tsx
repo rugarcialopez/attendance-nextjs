6467d367c88a64d2d144e4db1c28e2c21233aa1d
Ruben Garcia Lopez <rugarcialopez@gmail.com>
2021-05-31 18:16:45 +0200

    Unit test for home page

---
+++ test/pages/index.test.tsx
@@ -0,0 +1,11 @@
+
+import { render, screen } from '@testing-library/react';
+import Home  from '../../pages/index';
+
+describe('Home page', () => {
+  test('renders Welcome to attendance app! as a text', () => {
+    render(<Home />);
+    const welcomeElement = screen.getByText('Welcome to attendance app!');
+    expect(welcomeElement).toBeInTheDocument();
+  });
+})

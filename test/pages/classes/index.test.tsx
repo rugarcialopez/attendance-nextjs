f6bb9f4e1abfad1e864543940ce822c13b0c6c34
Ruben Garcia Lopez <rugarcialopez@gmail.com>
2021-06-01 18:09:34 +0200

    Increase coverage

--- test/pages/classes/index.test.tsx
+++ test/pages/classes/index.test.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from '@testing-library/react';
+import { waitFor, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Classes from '../../../pages/classes';
 describe('Classes page', () => {
   test('renders No classes found! as a text when there are no classes', () => {
@@ -15,14 +16,14 @@ describe('Classes page', () => {
     expect(addClasslement).toBeInTheDocument();
   });
 
-  test('renders the classes names when there are classes', () => {
+  test('renders classes names when there are classes', () => {
     const classes = [
       {
         id: '60b4b2952d77d67c70e47f11',
         subject: 'English',
         startDate: 'Wed May 05 2021',
         endDate: 'Tue May 25 2021',
-        attendanceState: ''
+        attendanceState: 'Pending approve'
       },
       {
         id: '60b4fc1ce1f29f0008c25ebe',
@@ -39,7 +40,7 @@ describe('Classes page', () => {
     expect(scienceSubjectElement).toBeInTheDocument();
   });
 
-  test('renders the classes dates when there are classes', () => {
+  test('renders classes dates when there are classes', () => {
     const classes = [
       {
         id: '60b4b2952d77d67c70e47f11',
@@ -65,6 +66,108 @@ describe('Classes page', () => {
     expect(scienceStartDateElement).toBeInTheDocument();
     const scienceEndDateElement = screen.getByText('Sun May 30 2021', { exact: false });
     expect(scienceEndDateElement).toBeInTheDocument();
+  });
+
+  describe('Students', () => {
+
+    beforeAll(() => {
+      window.fetch = jest
+        .fn()
+        .mockResolvedValueOnce({
+          json: async () => { return  { isLoggedIn: true, id: '60af9d9201351e3bee93f95a', fullName: 'learner one', role: 'student' } }
+        })
+        .mockResolvedValueOnce({
+          ok: true,
+          json: async () => { return  { message: 'success' } }
+        });
+
+    });
+
+    test('renders attend button', async () => {
+      const classes = [
+        {
+          id: '60b4b2952d77d67c70e47f11',
+          subject: 'English',
+          startDate: 'Wed May 05 2021',
+          endDate: 'Tue May 25 2021',
+          attendanceState: ''
+        }
+      ];
+      render(<Classes classList={classes}/>);
+      await waitFor(() => {
+        expect(screen.getByText('Attended')).toBeInTheDocument();
+      });
+    });
+
+    test('renders disabled attend button if student click on attend button', async () => {
+      const classes = [
+        {
+          id: '60b4b2952d77d67c70e47f11',
+          subject: 'English',
+          startDate: 'Wed May 05 2021',
+          endDate: 'Tue May 25 2021',
+          attendanceState: ''
+        }
+      ];
+      render(<Classes classList={classes} />);
+      await waitFor(() => {
+        expect(screen.getByText('Attended')).toBeInTheDocument();
+      });
+      expect(screen.getByText('Attended')).not.toBeDisabled();
+      const attendButton = screen.getByText('Attended');
+      userEvent.click(attendButton);
+      expect(screen.getByText('Attended')).toBeDisabled();
+    });
+
+    test('renders disabled attend button when student has attended the course', async () => {
+      const classes = [
+        {
+          id: '60b4b2952d77d67c70e47f11',
+          subject: 'English',
+          startDate: 'Wed May 05 2021',
+          endDate: 'Tue May 25 2021',
+          attendanceState: 'Pending approve'
+        }
+      ];
+      render(<Classes classList={classes}/>);
+      await waitFor(() => {
+        expect(screen.getByText('Attended')).toBeDisabled();
+      });
+    });
+
+    test('does not render edit button', async () => {
+      const classes = [
+        {
+          id: '60b4b2952d77d67c70e47f11',
+          subject: 'English',
+          startDate: 'Wed May 05 2021',
+          endDate: 'Tue May 25 2021',
+          attendanceState: 'Pending approve'
+        }
+      ];
+      render(<Classes classList={classes}/>);
+      await waitFor(() => {
+        expect(screen.getByText('Attended')).toBeDisabled();
+      });
+      expect(screen.queryByText('Edit')).not.toBeInTheDocument();
+    });
+
+    test('does not render approve/reject button', async () => {
+      const classes = [
+        {
+          id: '60b4b2952d77d67c70e47f11',
+          subject: 'English',
+          startDate: 'Wed May 05 2021',
+          endDate: 'Tue May 25 2021',
+          attendanceState: 'Pending approve'
+        }
+      ];
+      render(<Classes classList={classes}/>);
+      await waitFor(() => {
+        expect(screen.getByText('Attended')).toBeDisabled();
+      });
+      expect(screen.queryByText('Approve/Reject')).not.toBeInTheDocument();
+    });
   });
 
 
